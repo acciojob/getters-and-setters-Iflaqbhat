377d5370c5123e6d3f21cf8140dac0746612e3ba
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-13 09:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 class Person {
-	constructor(name, age) {
-		this.name = name;
-		this.age = age;
-	}
+  // Private fields
+  #name; // Private variable for name
+  #age;  // Private variable for age
 
-	getName() {
-		return this.name;
-	}
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
 
-	setAge(age) {
-		this.age = age;
-	}
+  // Getter for name
+  get name() {
+    return this.#name;
+  }
+
+  // Setter for age
+  set age(value) {
+    this.#age = value;
+  }
+
+  // Getter for age
+  get age() {
+    return this.#age;
+  }
 }
 
 class Student extends Person {
-	constructor(name, age) {
-		super(name, age); // Call the parent class constructor
-	}
+  constructor(name, age) {
+    super(name, age); // Call the parent class constructor to set name and age
+  }
 
-	study() {
-		console.log(`${this.name} is studying`);
-	}
+  study() {
+    console.log(`${this.name} is studying`); // Accessing #name via getter
+  }
 }
 
 class Teacher extends Person {
-	constructor(name, age) {
-		super(name, age); // Call the parent class constructor
-	}
+  constructor(name, age) {
+    super(name, age); // Call the parent class constructor to set name and age
+  }
 
-	teach() {
-		console.log(`${this.name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`); // Accessing #name via getter
+  }
 }
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
-
